b9edd7fd692212d53fc445f72104c7c64a438a41
tedraykov <tedraykov@gmail.com>
2021-12-01 22:56:41 +0200

    ci: jest and babel config
    Signed-off-by: tedraykov <tedraykov@gmail.com>

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,1 +1,28 @@
-module.exports = require("./node_modules/@reactioncommerce/api-utils/lib/configs/babel.config.cjs");
+/**
+ * Babel is used only for running Jest tests in API projects.
+ * If Jest adds support for ESM and import.meta, then Babel
+ * may become unnecessary.
+ */
+
+module.exports = function (api) { // eslint-disable-line no-undef
+  api.cache(false);
+
+  return {
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          targets: {
+            node: "12"
+          }
+        }
+      ]
+    ],
+    plugins: [
+      "babel-plugin-transform-import-meta",
+      "module:@reactioncommerce/babel-remove-es-create-require",
+      "rewire-exports",
+      "transform-es2015-modules-commonjs"
+    ]
+  };
+};
